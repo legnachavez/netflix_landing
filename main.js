f1377fa14557fda59294b5804fc5ddde8906c4ce
legna chavez <legna.ch18@gmail.com>
2022-01-12 23:27:59 -0500

    js

--- main.js
+++ main.js
@@ -9,8 +9,47 @@ function selectItem(e) {
 }
 
 function removeBorder() {
-  tabItems.forEach(item => item.classList.remove('tab-border'))
+  tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 
 //Listen for tab click
-tabItems.forEach(item => item.addEventListener('click', selectItem));
+tabItems.forEach(item =>
+  {item.addEventListener('click', selectItem);
+});
+
+
+
+
+
+
+// Select tab content item
+function selectItem(e) {
+  // Remove all show and border classes
+  removeBorder();
+  removeShow();
+  // Add border to current tab item
+  this.classList.add('tab-border');
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+  tabItems.forEach(item => {
+    item.classList.remove('tab-border');
+  });
+}
+
+// Remove show class from all content items
+function removeShow() {
+  tabContentItems.forEach(item => {
+    item.classList.remove('show');
+  });
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+  item.addEventListener('click', selectItem);
+});
